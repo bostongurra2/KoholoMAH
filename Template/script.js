9bd87fdc22305b90f7f921cffe8c4dcd85b1b30b
bostongurra2 <simme.norberg@gmail.com>
2017-04-19 13:45:03 +0200

    Lagt till nytt plugin, byggt karuseller + nytt färgschema

---
+++ Template/script.js
@@ -0,0 +1,39 @@
+var $st = $('.pagination');
+var $slickEl = $('.center');
+
+$slickEl.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+  var i = (currentSlide ? currentSlide : 0) + 1;
+  $st.text(i + ' of ' + slick.slideCount);
+});
+
+$slickEl.slick({
+  centerMode: true,
+  centerPadding: '100px',
+  slidesToShow: 3,
+  focusOnSelect: true,
+  dots: false,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  arrows: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
